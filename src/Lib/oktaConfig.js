0ffcd9c2df17f4061eaab676cf5c8357a80768d7
theUmeshC <umesh.c@celestialsys.com>
2023-03-10 20:24:06 +0530

    ec2

--- src/Lib/oktaConfig.js
+++ src/Lib/oktaConfig.js
@@ -3,7 +3,7 @@ export const oktaConfig = {
   issuer: `https://dev-21034974.okta.com/oauth2/default`,
   redirectUri: `http://65.1.3.29:3000/login/callback`, // this makes it so redirects to login if not logged in for secure routes
   scopes: ["openid", "profile", "email"],
-  pkce: false,
+  pkce: true,
   disableHttpsCheck: true,
   features: {
     idpDiscovery: true
